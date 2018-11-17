990d863553a5e32d8b7c06f377cbb28acadf45dc
liulina <1005490286@qq.com>
2018-11-17 14:36:04 +0800

    项目初始化

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// import HelloWorld from '@/components/HelloWorld'
+
+import Layout from '@/views/layout/Layout'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Layout',
+      component: Layout
+    }
+  ]
+})
---
+++ src/views/layout/Layout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div :class="classObj" class="app-wrapper"></div>
+</template>
+<script>
+export default {
+  name: 'Layout'
+}
+</script>
+<style rel="stylesheet/scss" lang="scss" scoped>
+
+</style>
+
